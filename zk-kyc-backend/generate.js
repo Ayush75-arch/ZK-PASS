64c9cb431fcbfbb0e162109cb7a1b54762f382e9
JeswinMickle <jeswinm37@gmail.com>
2026-04-03 18:04:51 +0000

    Added only zk-proof folder

--- zk-kyc-backend/generate.js
+++
@@ -1,6 +0,0 @@
-const algosdk = require("algosdk");
-
-const acc = algosdk.generateAccount();
-
-console.log("Address:", algosdk.encodeAddress(acc.addr.publicKey));
-console.log("Mnemonic:", algosdk.secretKeyToMnemonic(acc.sk));
